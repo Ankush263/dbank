6bc011ceca7291071a8de4ad90bfbd3d68d59577
Ankush263 <ankushbanik263@gmail.com>
2022-05-20 11:06:28 +0530

    Add withdrawl functionality

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -12,15 +12,21 @@ document.querySelector("form").addEventListener("submit", async (event) => {
   const button = event.target.querySelector("#submit-btn")
 
   const topupAmount = parseFloat(document.getElementById("input-amount").value)
-  const withdrawlAmount = document.getElementById("withdrawal-amount").value
+  const withdrawlAmount = parseFloat(document.getElementById("withdrawal-amount").value)
 
-  button.setAttribute("disabaled", true)
+  button.setAttribute("disabled", true)
 
-  await dbank.topUp(topupAmount)
+  if(document.getElementById("input-amount").value.length != 0){
+    await dbank.topUp(topupAmount)
+  }
+  if(document.getElementById("withdrawal-amount").value.length != 0){
+    await dbank.withdraw(withdrawlAmount)
+  }
 
   const currentAmount = await dbank.checkBalance()
   document.getElementById("value").innerText = currentAmount.toFixed(2)
 
   document.getElementById("input-amount").value = ""
+  document.getElementById("withdrawal-amount").value = ""
   button.removeAttribute("disabled")
 })
