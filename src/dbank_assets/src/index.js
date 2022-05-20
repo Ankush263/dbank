46e4bfe3c5ab23a862c187d57231f4fa04dea18b
Ankush263 <ankushbanik263@gmail.com>
2022-05-20 09:54:20 +0530

    add topUp functionality

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -4,3 +4,23 @@ window.addEventListener("load", async () => {
   const currentAmount = await dbank.checkBalance()
   document.getElementById("value").innerText = currentAmount.toFixed(2)
 })
+
+document.querySelector("form").addEventListener("submit", async (event) => {
+  event.preventDefault()
+  // console.log(topupAmount)
+
+  const button = event.target.querySelector("#submit-btn")
+
+  const topupAmount = parseFloat(document.getElementById("input-amount").value)
+  const withdrawlAmount = document.getElementById("withdrawal-amount").value
+
+  button.setAttribute("disabaled", true)
+
+  await dbank.topUp(topupAmount)
+
+  const currentAmount = await dbank.checkBalance()
+  document.getElementById("value").innerText = currentAmount.toFixed(2)
+
+  document.getElementById("input-amount").value = ""
+  button.removeAttribute("disabled")
+})
